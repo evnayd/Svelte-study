b258abd6ad9c513abf2bac8f8265db6312426732
evnayd <vjane87@yahoo.com>
2021-02-04 15:03:27 +0100

    try to include tailwind in project

--- src/App.svelte
+++ src/App.svelte
@@ -57,29 +57,33 @@ function handleOnSubmit() {
 
 </script>
 
-	<div class="container">
-<h1>Change password</h1>
+	<div class="container mx-auto">
+		<div class="bg-white w-full  lg:w-1/2 mx-auto shadow-md p-6 xl:p-14 text-left sm:text-center my-16 lg:my-32">
+<h1 class="font-black text-xl sm:text-4xl mb-2 sm:mb-8">Change password</h1>
 <p>Please enter a new password to complete the password recovery process.</p>
-<div>
-<input type={inputType}   on:input={(e) => (value = e.target.value)} id ="password" />
-<button on:click={hideValue}>HIDE</button>
+<div class="relative">
+<input class="text-gray-500 text-lg w-full border-b-2 mb-2 p-1 outline-none overflow-hidden" type={inputType}   on:input={(e) => (value = e.target.value)} id ="password" />
+<button class="absolute bottom-3 right-1 text-gray-300 hover:text-gray-400 focus: outline-none focus: border-none" on:click={hideValue}>HIDE</button>
 </div>
-<ul>
-	<li class:active ={!lengthError}>8-64 characters</li>
-	<li class:active ={!lowerCaseError}>1 Lowercase</li>
-	<li class:active ={!upperCaseError}>1 Uppercase</li>
-	<li class:active ={!numberError}>1 Number</li>
+<ul class="flex flex-col sm:flex-row justify-between text-sm mb-6 flex-nowrap">
+	<li class="w-3/5 sm:w-auto bg-gray-300  rounded-lg px-3.5  py-2 text-gray-500  m-1  text-left sm:text-center break-normal flex-auto"  class:active ={!lengthError}>8-64 characters</li>
+	<li class="w-3/5 sm:w-auto bg-gray-300  rounded-lg px-3.5  py-2 text-gray-500  m-1  text-left sm:text-center break-normal flex-auto"  class:active ={!lowerCaseError}>1 Lowercase</li>
+	<li class="w-3/5 sm:w-auto bg-gray-300  rounded-lg px-3.5  py-2 text-gray-500  m-1  text-left sm:text-center break-normal flex-auto"  class:active ={!upperCaseError}>1 Uppercase</li>
+	<li class="w-3/5 sm:w-auto bg-gray-300  rounded-lg px-3.5  py-2 text-gray-500  m-1  text-left sm:text-center break-normal flex-auto"  class:active ={!numberError}>1 Number</li>
 	
 </ul>
-<button type ="submit" on:click={handleOnSubmit}> Confirm </button>
+<button class="w-full bg-purple-700 hover:bg-purple-900 rounded-md p-4 text-white shadow-md font-semibold text-sm  sm:text-xl overflow-hidden"  type ="submit" on:click={handleOnSubmit}> Confirm </button>
+</div>
 </div>
 
 
 
-<style>
-	h1 {
-		color: green;
-	}
+<style global lang="postcss">
+
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
 
     .active {
 		outline : green 2px solid;
